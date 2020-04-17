41d4b69b0f2fad80247b24aa7eb768ee7554e61d
Krishan Kant Sharma <krishansharma0394@gmail.com>
2020-04-17 10:44:58 -0500

    Update

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,25 @@
- var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+/*var n = localStorage.getItem('on_load_counter');
+ 
+if (n === null) {
+    n = 0;
+}
+ 
+n++;
+ 
+localStorage.setItem("on_load_counter", n);
+ 
+document.getElementById('value').innerHTML = n;
+   */
+var x = localStorage.getItem('on_load_counter');
+if (x === null) {
+    x = 0;
+}
+
+
+var element = document.getElementById("value");
+element.innerHTML = x;
+
+function button1() {
+    element.innerHTML = ++x;
+    localStorage.setItem("on_load_counter", x);
+}
